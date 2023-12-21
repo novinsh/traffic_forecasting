a9f2da7c8b25399d4736b42d2cfed5df88c0197b
Novin Shahroudi <novinsha@gmail.com>
2023-12-21 12:34:40 +0200

    Add scripts for data downloading and setup.

---
+++ download_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# we store the data files on dropbox. this script downloads them to the root of the project.
+# data
+wget "https://www.dropbox.com/scl/fi/nzo0r46xf1px5r37n96h5/aggregated.zip?rlkey=hhehoy72utuz8s1j7dfhag66x&dl=0" -O data_small.zip
+wget "https://www.dropbox.com/scl/fi/vjch1875tn6qyrxglpmy0/data.zip?rlkey=wt18fzfj8054v4q219eva4xvd&dl=0" -O data_large.zip
+
+# edge info
+wget "https://www.dropbox.com/scl/fi/4thhh8yjhh8zagtpxb2cr/edgeinfo.csv?rlkey=wxb87kbvzwvn6s74r89q5ay41&dl=0" -O edge_info.csv
+wget "https://www.dropbox.com/scl/fi/itb2iv38cqhnf74cz7zv0/tartu_edge.csv?rlkey=gmycfuiutcv0xcy5yeka6wq6c&dl=0" -O tartu_edge.csv
+
+# saved models
+# download saved models to reuse
+wget "https://www.dropbox.com/scl/fi/f3y2xj662yfam8de63zu5/cluster_model.pkl?rlkey=msfffwfam06d41ruw0besa4ox&dl=0" -O cluster_model.pkl
---
+++ prepare_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" = "clean" ]; then
+    echo "Performing clean operation..."
+    # delete downloaded files and folders
+    rm aggregated -r
+    rm data -r
+    rm data_large.zip
+    rm data_small.zip
+    rm edge_info.csv
+    rm tartu_edge.csv
+    rm cluster_model.pkl
+else
+    echo "Performing data operations..."
+    # Unzip and preliminary data preparation 
+    unzip data_small.zip
+    unzip data_large.zip
+fi
